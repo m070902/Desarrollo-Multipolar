e07b974033b7ad4c9bf94481f0dec00e201feffd
Marcos Sarmiento Pellitero <marcos7902@hotmail.com>
2026-06-16 11:14:38 +0200

    Prueba de github y comits en vscode

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 ///Autor: Marcos Sarmiento Pellitero
 //Fecha de creación: 26 de octubre de 2023
 
-//Descripción: En este código se va a tratar de realizar un programa que permita introducir una distribución 
-//de cargas puntuales en un espacio y permita calcular los momentos dipolares correspondientes a dicha 
-//distribución
-
 //Sección de paquetes empleados
 
 #include <iostream>
@@ -27,9 +23,9 @@ void establecer_vectores(matrix& mat, int n){
     std::cout << "\nIntroduzca las coordenadas cartesianas atribuidas a la " << n+1 << "º partícula.\n" << std::endl;
 
     for (i = 1; i < 4; i++){
-        
+
         std::cin >> mat[i][n];
-          
+
     }
 
 }
@@ -37,11 +33,11 @@ void establecer_vectores(matrix& mat, int n){
 //Se calcula el momento monopolar. Para ello se suma el valor de las cargas de todas las partículas.
 
 int calc_mom_monopolar(matrix mat, int n){
-    
+
     int mom_monopolar = 0, i;
 
     for (i=0; i<n; i++){
-        
+
         mom_monopolar = mom_monopolar + mat[0][i];
 
     }
@@ -50,7 +46,7 @@ int calc_mom_monopolar(matrix mat, int n){
 
 }
 
-//Se calcula el momento dipolar. 
+//Se calcula el momento dipolar.
 
 rows calc_mom_dipolar(matrix mat, int n){
 
@@ -83,7 +79,7 @@ matrix mom_cuadrupolar(matrix mat, int n){
                 if (i == j) {
 
                     matriz_mom_cuadrupolar[j-1][i-1] = matriz_mom_cuadrupolar[j-1][i-1] + mat[0][k]*(3*mat[i][k]*mat[j][k] - (mat[1][k]*mat[1][k]+mat[2][k]*mat[2][k]+mat[3][k]*mat[3][k]));
-                    
+
                 }else{
 
                     matriz_mom_cuadrupolar[j-1][i-1] = matriz_mom_cuadrupolar[j-1][i-1] + mat[0][k]*(3*mat[i][k]*mat[j][k]);
@@ -116,11 +112,11 @@ int main(){
     std::vector<rows> all_vectors(4,rows(numero_part));
 
     //Se llama a la función correspondiente para poder designar las componentes de cada vector
-    
+
     for(i=0; i<numero_part; i++){
 
         establecer_vectores(all_vectors, i);
-        
+
     }
 
     //Se llama a la función correspondiente para calcular el momento monopolar
@@ -128,13 +124,13 @@ int main(){
     std::cout << "\nEl momento monopolar del sistema será de: " << calc_mom_monopolar(all_vectors, numero_part) << std::endl << std::endl;
 
     //Se llama a la función correspondiente para calcular el momento dipolar
-    
+
     mom_dipolar = calc_mom_dipolar(all_vectors, numero_part);
 
     std::cout << "El momento dipolar del sistema será: q x [(" << mom_dipolar[0] << ") x u_x + ("<< mom_dipolar[1] << ") x u_y + ("<< mom_dipolar[2] << ") x u_z]\n" << std::endl;
-    
+
     //Se llama a la función correspondiente para calcular el momento cuadrupolar
-    
+
     mat_mom_cuadrupolar = mom_cuadrupolar(all_vectors, numero_part);
 
     std::cout << "El momento cuadrupolar del sistema vendrá dado por el siguiente tensor:\n" << std::endl;
